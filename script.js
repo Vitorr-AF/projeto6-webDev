47bc57e091a1d0279afa46d9d51be98930ad23f8
Vitorr-AF <vitoraugustofranca7@gmail.com>
2024-04-19 10:22:29 -0300

    Projeto 6

---
+++ script.js
@@ -0,0 +1,37 @@
+// VALIDAÇÃO DE LOGIN
+
+function validar(){
+
+    //declarando as variaveis
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    if(usuario === "Admin" && senha === "12345"){
+        window.open("dashboard.html")
+    }else{
+        alert("usuario ou senha inválidos")
+    }
+}
+
+
+
+
+// CRIANDO UM SLIDE SHOW AUTOMÁTICO
+
+// DECLARANDO AS VARIÁVEIS
+let imagens = ['./img/Carro1.jpg','./img/Carro2.jpg','./img/Carro3.jpg'];
+let index=0;
+let time = 3000;
+
+function slideShow(){
+    document.getElementById("imgbanner").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout('slideShow()', time);
+
+}
+
+slideShow();
